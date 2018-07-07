6e9a54246ace30008995253fd7bfcb87566df30f
Zack <zixux@hotmail.com>
2018-07-07 22:14:47 +0100

    new fields

--- backend/data.py
+++ backend/data.py
@@ -1,26 +1,32 @@
 proposals = [
     dict(
+        id=0,
         title="learn ARG-101 and start a wheat farm",
         beneficiaries=['Zoe Xia', 'Andre Monty', 'Steph Mo'],
         description='We are going to complete the "Introduction to Arguculture" course and start a wheat farm.',
         course='ARG-101',
         amount_per_person=[100, 110, 80],
+        amount=290,
         voted=False
     ),
     dict(
+        id=1,
         title="Expand our fishing business with the advanced fishing course",
         beneficiaries=['Sot Nao', 'Eliz G'],
         description='We are aiming to expanding our fishing business and we will first learn "Extreme Fishing"',
         course='FISH-402',
         amount_per_person=[300, 300],
+        amount=600,
         voted=False
     ),
     dict(
+        id=2,
         title="Learn society's value and culture",
         beneficiaries=['Sot Nao'],
         description='We are aiming to expanding our fishing business and we will first learn "Extreme Fishing"',
         course='SOC-201',
         amount_per_person=[200],
+        amount=200,
         voted=False
     )
 ]
