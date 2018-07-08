2d73c87209c67ff8bd850dbac2bcd558939d259f
Stephane Moreau <st.moreau@outlook.com.gr>
2018-07-08 14:36:44 +0100

    logo

--- client/src/components/Menu/Menu.js
+++ client/src/components/Menu/Menu.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReduxMenu from './reduxMenu';
+import logo from '../../images/logo.png';
 
 const Menu = () => (
   <ReduxMenu>
+    <img
+      src={logo}
+      style={{
+        width: '80px',
+        marginLeft: '15px'
+      }}
+    />
     <a id="home" className="menu-item menu-link" href="/">
       Home
     </a>
