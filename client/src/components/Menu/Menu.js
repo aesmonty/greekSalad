667e7804e68758fec2c416a50b2094833a16a7b6
monty <amonteolivam@gmail.com>
2018-07-08 15:12:27 +0100

    menu items

--- client/src/components/Menu/Menu.js
+++ client/src/components/Menu/Menu.js
@@ -20,6 +20,20 @@ const Menu = () => (
     <a id="create-pool" className="menu-item menu-link" href="/new-proposal">
       New proposal
     </a>
+
+    <a id="find-course" className="menu-item menu-link">
+      Find me a course
+    </a>
+
+    <a id="bazaar" className="menu-item menu-link">
+      Open Bazaar
+    </a>
+
+    <a id="qi" className="menu-item menu-link">
+      Qi Exchange
+    </a>
+
+
   </ReduxMenu>
 );
 
