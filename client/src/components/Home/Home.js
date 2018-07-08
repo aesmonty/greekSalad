9cd4456f696fc7ab3e7a9af3809ef6a2027d7ab4
Stephane Moreau <st.moreau@outlook.com.gr>
2018-07-08 01:33:55 +0100

    proposals section

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,17 +1,34 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Card, Col, Row, Divider } from 'antd';
-// import axios from 'axios';
+import { Card, Col, Row, Divider, List, Icon } from 'antd';
+import axios from 'axios';
 import { EthereumIcon } from '../Web3/EthereumIcon';
 import { HomeMiddleBanner } from './HomeMiddleBanner';
 
 class Home extends Component {
+  state = {
+    proposals: []
+  };
   componentDidUpdate(prevProps) {
+    let self = this;
     if (this.props.id !== prevProps.id) {
       console.log(`make a GET to the API sending id: ${this.props.id}`);
+      axios
+        .get('http://35.206.132.245:8020/proposals')
+        .then(function(response) {
+          self.setState({ proposals: response.data });
+        })
+        .catch(function(error) {
+          console.log(error);
+        })
+        .then(function() {
+          // always executed
+        });
     }
   }
-
+  handleVoteClick(id) {
+    console.log(`use web3 with the id of ${id} and setState accordingly`);
+  }
   render() {
     return (
       <div style={{ background: '#ECECEC', padding: '30px' }}>
@@ -26,8 +43,35 @@ class Home extends Component {
         <div>
           <Row gutter={16}>
             <Col span={12}>
-              <Card title="Current proposal" bordered={false}>
-                Card content
+              <Card title="Current proposals" bordered={false}>
+                <List
+                  itemLayout="horizontal"
+                  dataSource={this.state.proposals}
+                  renderItem={item => (
+                    <List.Item>
+                      <List.Item.Meta
+                        title={<a>{item.title}</a>}
+                        description={
+                          <div>
+                            <span style={{ marginRight: 10, float: 'left' }}>
+                              {item.voted === true ? (
+                                <Icon style={{ color: 'green' }} type="up-circle" />
+                              ) : (
+                                <Icon
+                                  data-id={item.id}
+                                  onClick={this.handleVoteClick.bind(null, item.id)}
+                                  type="up-circle"
+                                />
+                              )}
+                            </span>
+                            <span style={{ width: '500px', float: 'left' }}>{item.description}</span>
+                            <span style={{ float: 'right' }}>{item.amount} Qi</span>
+                          </div>
+                        }
+                      />
+                    </List.Item>
+                  )}
+                />
               </Card>
             </Col>
             <Col span={12}>
